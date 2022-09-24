b707093a46ae29f0792a572bd0b7adc04db443fa
Chineze Ogugua <christabelogugua@gmail.com>
2022-09-24 06:14:11 -0700

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-def print_arg(argv):
-    if __name__ == "__main__":
-        """Print the number of and list of arguments."""
-        import sys
-
-        count = len(sys.argv)
-        print("{} ".format(args - 1), end="")
-        if count == 1:
-            print("arguments.")
-        elif count > 1:
-            if count == 2:
-                print("argument:")
-            elif count > 2:
-                print("arguments:")
-            for i in range(count - 1):
-                print("{}: {}".format(i + 1, sys.argv[i + 1]))
+if __name__ == "__main__":
+    import sys
+    args = len(sys.argv)
+    print("{} ".format(args - 1), end="")
+    if args == 1:
+        print("arguments.")
+    elif args > 1:
+        if args == 2:
+            print("argument:")
+        elif args > 2:
+            print("arguments:")
+        for i in range(args - 1):
+            print("{}: {}".format((i + 1), sys.argv[i + 1]))
