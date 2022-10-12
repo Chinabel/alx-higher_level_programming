e4d57fa6b26228a78ec5eb76fcad10d89db9a216
Chineze Ogugua <christabelogugua@gmail.com>
2022-10-11 23:04:54 -0700

    square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,15 +1,34 @@
-i#!/usr/bin/python3
-"""Creating a new class called Square"""
+#!/usr/bin/python3
+Square = __import__('2-square').Square
 
+my_square_1 = Square(3)
+print(type(my_square_1))
+print(my_square_1.__dict__)
 
-class Square:
-    """class Square that defines a square by: (based on 1-square.py)"""
-    def __init__(self, size=0):
-        """Initialize Square with size attribute"""
-        self.__size = size
+my_square_2 = Square()
+print(type(my_square_2))
+print(my_square_2.__dict__)
 
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e)
 
-        if size < 0:
-            raise ValueError("size must be >= 0")
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_3 = Square("3")
+    print(type(my_square_3))
+    print(my_square_3.__dict__)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_4 = Square(-89)
+    print(type(my_square_4))
+    print(my_square_4.__dict__)
+except Exception as e:
+    print(e)
