16ebaf8034e0ab96da1c5d5259007fc0271f0050
Chineze Ogugua <christabelogugua@gmail.com>
2022-10-18 07:18:55 -0700

    magic string

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    setattr(magic_string, "prnt", getattr(magic_string, "prnt", - 1) + 1)
-    return "Holberton" + ", Holberton" * magic_string.prnt
+    magic_string.count = getattr(magic_string, 'count', 0) + 1
+    return ", ".join(["BestSchool" for i in range(magic_string.count)])
