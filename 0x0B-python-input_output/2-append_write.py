b0032aece8667eae0e7240f49aa255075f3c784c
Chineze Ogugua <christabelogugua@gmail.com>
2022-10-25 14:02:43 -0700

    append write

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,21 +1,9 @@
 #!/usr/bin/python3
-"""read_lines
-"""
-import os
+"""This module defines a file-appending function."""
 
 
-def read_lines(filename="", nb_lines=0):
-    """Takes in str filename to read, and n lines w/ int nb_lines
+def append_write(filename="", text=""):
+    """Appends a string to the end of a UTF8 text file
     """
-
-
-    with open(filename, encoding="utf-8") as readFile:
-        lineNum = 0
-        while True:
-            line = readFile.readline()
-            lineNum += 1
-            print(line, end='')
-            if lineNum >= nb_lines and nb_lines > 0:
-                break
-            if not line:
-                break
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
