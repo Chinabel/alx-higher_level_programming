f2d3c741cc40ce937f9856f9959cd128f0517a51
Chineze Ogugua <christabelogugua@gmail.com>
2023-01-17 05:24:36 -0800

    task 0 check

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""Fetches a URL."""
-import urllib.request as request
+import urllib.request
 
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    content = response.read()
+    utf8_content = content.decode('utf-8')
 
-if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as response:
-        if response.readable():
-            data = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(data)))
-            print("\t- content: {}".format(data))
-            print("\t- utf8 content: {}".format(data.decode("utf-8")))
+print('Body response:')
+print('\t- type: {}'.format(type(content)))
+print('\t- content: {}'.format(content))
+print('\t- utf8 content: {}'.format(utf8_content))
