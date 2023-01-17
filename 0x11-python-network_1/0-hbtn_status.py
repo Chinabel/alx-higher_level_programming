cdc1feb2482a5aa284a0c007bfacf9cdd59e5f90
Chineze Ogugua <christabelogugua@gmail.com>
2023-01-17 05:43:37 -0800

    task 1 fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+"""urllib usage"""
 import urllib.request
+import urllib.parse
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    content = response.read()
-    utf8_content = content.decode('utf-8')
 
-print('Body response:')
-print('\t- type: {}'.format(type(content)))
-print('\t- content: {}'.format(content))
-print('\t- utf8 content: {}'.format(utf8_content))
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type:', type(html))
+        print('\t- content:', html)
+        print('\t- utf8 content:', html.decode('UTF-8'))
