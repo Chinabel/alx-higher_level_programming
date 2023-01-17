377d03ec13a4fa7c6e1d9f36f7d57cbded70260b
Chineze Ogugua <christabelogugua@gmail.com>
2023-01-17 05:28:23 -0800

    task 4 fix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""Fetches a URL."""
+"""Python script that fetches https://intranet.hbtn.io/status"""
+
 import requests
 
+if __name__ == "__main__":
 
-if __name__ == '__main__':
-    response = requests.get('https://intranet.hbtn.io/status')
+    url = 'https://intranet.hbtn.io/status'
+
+    req = requests.get(url)
     print('Body response:')
-    print('\t- type: {}'.format(type(response.text)))
-    print('\t- content: {}'.format(response.text))
+    print('\t- type: {}'.format(type(req.text)))
+    print('\t- content: {}'.format(req.text))
